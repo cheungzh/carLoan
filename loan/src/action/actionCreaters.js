cf15cc81886fa9567422213fe2fdf6bee39782b3
guoweiwei <guoweiwei@zafh.com.cn>
2018-03-02 17:09:21 +0800

    购物车结算action

--- loan/src/action/actionCreaters.js
+++ loan/src/action/actionCreaters.js
@@ -2,6 +2,19 @@ import Tool from '../Tool/Tool';
 export function fetchProduct (params={}) {
   return function (dispatch) {
     dispatch({type: 'FETCH_PRODUCT'});
+    /*
+    * 模拟数据
+    **/
+    /*let json = [
+      {id:1,productPic: 'http://www.givenchybeauty.cn/files/2017/06/rougeinterdit.jpg',productName: '禁忌之吻霓虹唇膏',price:'303',sale:120,stock: 8},
+      {id:2,productPic: 'http://www.givenchybeauty.cn/files/2017/12/le-rouge-sculpt-pro.jpg',productName: '纪梵希－高定香榭双色唇膏',price:'233',sale:200,stock: 2385},
+      {id:3,productPic: 'https://gd3.alicdn.com/imgextra/i2/1970926915/TB2vxNUinnI8KJjSszbXXb4KFXa_!!1970926915.jpg_400x400.jpg',productName: 'YSL/圣罗兰黑管漆光唇釉黑管唇釉',price:'260',sale:113,stock: 773},
+      {id:4,productPic: 'https://gd3.alicdn.com/imgextra/i2/739171675/TB2ePpWedLO8KJjSZFxXXaGEVXa_!!739171675.jpg_400x400.jpg',productName: 'Tom Ford/汤姆福特黑金黑管哑光唇膏matte口红',price:'305',sale:120,stock: 234},
+      {id:5,productPic: 'http://www.givenchybeauty.cn/files/2017/06/rougeinterdit.jpg',productName: '禁忌之吻霓固定',price:'233',sale:145,stock: 24},
+      {id:6,productPic: 'http://www.givenchybeauty.cn/files/2017/06/rougeinterdit.jpg',productName: '禁忌之吻霓虹唇膏',price:'303',sale:220,stock: 17},
+      {id:7,productPic: 'https://gd3.alicdn.com/imgextra/i2/739171675/TB2ePpWedLO8KJjSZFxXXaGEVXa_!!739171675.jpg_400x400.jpg',productName: '禁忌之吻霓虹唇膏',price:'303',sale:45,stock: 56},
+    ];
+    dispatch({type: 'RECEIVE_PRODUCT',payload:json});*/
     Tool.mFetch({
       url:'/api/product/list',
       data: params
@@ -30,9 +43,11 @@ export const addToCart = productId => (dispatch,getState) => {
   }
 };
 
-//选中/取消 购物车数据结算
+/*
+*选中/取消 购物车数据结算
+* */
 export const checkItem = (productId) => (dispatch,getState) => {
-  let index = getState().cartList.chooseIds.indexOf(productId)
+  let index = getState().cartList.chooseIds.indexOf(productId);
   if( index !== -1) {
     dispatch({
       type: 'CHECK_CANCEL',
@@ -44,5 +59,41 @@ export const checkItem = (productId) => (dispatch,getState) => {
       productId
     })
   }
+};
+
+/*
+* 全选/取消 购物车数据结算
+* */
+export const selectAll = (flag) => (dispatch, getState) => {
+  if(flag) {
+    let productIds = getState().cartList.addedIds;
+    dispatch({
+      type: 'CHECK_IN_ALL',
+      productIds
+    })
+  }else {
+    dispatch({
+      type: 'CHECK_CANCEL_ALL'
+    })
+  }
+};
+
+/*
+* 购物车结算功能
+* */
+export const checkOut = () => (dispatch,getState) => {
+  let { chooseIds } = getState().cartList;
+  if(chooseIds.length === 0) {
+    return Tool.tip({
+      message: '请先选中要购买的商品'
+    })
+  }
+  dispatch({
+    type: 'CHECK_OUT_CART',
+    checkOutIds: chooseIds
+  });
+  dispatch({
+    type: 'CHECK_CANCEL_ALL'
+  })
 };
 
--- loan/src/action/actions.js
+++ loan/src/action/actions.js
@@ -1,19 +1,21 @@
-/**
- * Created by apple on 18/1/30.
- */
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOADING = 'LOADING';
 export const EXIT = 'EXIT';
+
 export const FETCH_PRODUCT = 'FETCH_PRODUCT';
 export const RECEIVE_PRODUCT = 'RECEIVE_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const DEC_FROM_CART = 'DEC_FROM_CART';
 
 export const ADD_COUNT = 'ADD_COUNT';
 export const UPDATE_CART = 'UPDATE_CART';
-export const CHECK_IN = 'CHECK_CART';
+export const CHECK_OUT_CART = 'CHECK_OUT_CART';
+export const CHECK_IN = 'CHECK_IN';
 export const CHECK_CANCEL = 'CHECK_CANCEL';
+export const CHECK_IN_ALL = 'CHECK_IN_ALL';
+export const CHECK_CANCEL_ALL = 'CHECK_CANCEL_ALL';
 
 
 //产品详情数据获取
