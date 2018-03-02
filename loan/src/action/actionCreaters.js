41061d7a13a06b834a398115f52d4b9400e16512
guoweiwei <guoweiwei@zafh.com.cn>
2018-03-02 09:40:02 +0800

    action

--- loan/src/action/actionCreaters.js
+++ loan/src/action/actionCreaters.js
@@ -1,5 +1,4 @@
 import Tool from '../Tool/Tool';
-
 export function fetchProduct (params={}) {
   return function (dispatch) {
     dispatch({type: 'FETCH_PRODUCT'});
@@ -11,3 +10,39 @@ export function fetchProduct (params={}) {
     });
   }
 }
+
+//获取购物车数据
+export function getCartList () {
+
+}
+
+
+//添加数据到购物车
+export const addToCart = productId => (dispatch,getState) => {
+  if(getState().cartList.productsById[productId].stock > 0) {
+    dispatch({
+      type: "ADD_TO_CART",
+      productId
+    });
+    Tool.tip({
+      message: '成功加入购物车,可去购物车查看'
+    });
+  }
+};
+
+//选中/取消 购物车数据结算
+export const checkItem = (productId) => (dispatch,getState) => {
+  let index = getState().cartList.chooseIds.indexOf(productId)
+  if( index !== -1) {
+    dispatch({
+      type: 'CHECK_CANCEL',
+      index
+    })
+  }else{
+    dispatch({
+      type: 'CHECK_IN',
+      productId
+    })
+  }
+};
+
--- loan/src/action/actions.js
+++ loan/src/action/actions.js
@@ -6,5 +6,18 @@ export const LOADING = 'LOADING';
 export const EXIT = 'EXIT';
 export const FETCH_PRODUCT = 'FETCH_PRODUCT';
 export const RECEIVE_PRODUCT = 'RECEIVE_PRODUCT';
-export const ADD_CART = 'ADD_CART';
+
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const DEC_FROM_CART = 'DEC_FROM_CART';
+
+export const ADD_COUNT = 'ADD_COUNT';
+export const UPDATE_CART = 'UPDATE_CART';
+export const CHECK_IN = 'CHECK_CART';
+export const CHECK_CANCEL = 'CHECK_CANCEL';
+
+
+//产品详情数据获取
+export const FETCH_PRODUCT_DETAIL = 'FETCH_PRODUCT_DETAIL';
+export const RECEIVE_PRODUCT_DETAIL = 'RECEIVE_PRODUCT_DETAIL';
+
 
