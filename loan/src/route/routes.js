f12c496ab6cbe8cccb7a0b76eeb77e0620b88cc3
guoweiwei <guoweiwei@zafh.com.cn>
2018-03-02 17:09:58 +0800

    路由修改

--- loan/src/route/routes.js
+++ loan/src/route/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route,Switch } from 'react-router-dom';
-import LoginFrom from '../component/login/LoginForm';
+import Login from '../component/login/login';
 import App from '../component/App';
 
 import ProductDetail from '../component/productDetail/ProductDetail';
@@ -8,7 +8,8 @@ export default class Routes extends React.Component{
   render () {
     return (
         <Switch>
-          <Route path="/user/login" component={LoginFrom} />
+          <Route path="/user/login/:url" component={Login} />
+          <Route path="/user/login" component={Login} />
           <Route path="/product/detail" component={ProductDetail} />
           <Route path="/" component={App}/>
         </Switch>
