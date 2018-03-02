8d85e14bfdac202a73e8010f2261a2c9e7dd6d23
guoweiwei <guoweiwei@zafh.com.cn>
2018-03-02 09:42:57 +0800

    项目入口文件

--- loan/src/main.js
+++ loan/src/main.js
@@ -7,7 +7,8 @@ import store from './store/store';
 import Routes from './route/routes';
 import Login from './component/login/login';
 // console.log(store.getState());
-
+import { fetchProduct } from './action/actionCreaters';
+store.dispatch(fetchProduct());
 ReactDom.render(
   <Provider store={store}>
     <BrowserRouter>
